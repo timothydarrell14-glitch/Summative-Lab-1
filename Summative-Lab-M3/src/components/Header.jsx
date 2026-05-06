84c652e905ca6970b20bc5d7dc007ae594b68e5c
timothydarrell14-glitch <timothydarrell14@gmail.com>
2026-05-06 08:39:53 +0300

    Refactor Header component to use destructured props; update App component to pass GitHub link; enhance header styles for better alignment

--- Summative-Lab-M3/src/components/Header.jsx
+++ Summative-Lab-M3/src/components/Header.jsx
@@ -7,22 +7,17 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 
 library.add(fas, far, fab)
 
-function Header(props) {
+function Header({name, git, portfolio}) {
 
-    function handleClick() {
-
-    }
     return (
         <header>
-            <h1>{props.name}</h1>
+            <h1>{name}</h1>
             <div>
                 <ul>
-                    <li><a className="list" href={props.portfolio} target="_blank" ><FontAwesomeIcon icon="fa-solid fa-user" style={{ color: "rgb(148, 114, 8)", }} /></a></li>
-                    <li><a className="list" href=""><FontAwesomeIcon icon="fa-brands fa-github" style={{ color: "rgb(148, 114, 8)", }} /></a></li>
+                    <li><a className="list" href={portfolio} target="_blank" ><FontAwesomeIcon icon="fa-solid fa-user" style={{ color: "rgb(148, 114, 8)", }} /></a></li>
+                    <li><a className="list" href={git} target='_blank'><FontAwesomeIcon icon="fa-brands fa-github" style={{ color: "rgb(148, 114, 8)", }} /></a></li>
                     <li><a className="list" href=""><FontAwesomeIcon icon="fa-solid fa-envelope" style={{ color: "rgb(148, 114, 8)", }} /></a></li>
                 </ul>
-                <button onClick={handleClick}><FontAwesomeIcon icon="fa-solid fa-moon" style={{ color: "rgb(148, 114, 8)", }} /></button>
-
             </div>
         </header>
     )
